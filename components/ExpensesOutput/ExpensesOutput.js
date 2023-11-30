bc67e75caedada83ac8f9c24b68c57710c2f922c
eraybuyukkanat <erybykknt@gmail.com>
2023-11-30 22:25:26 +0300

    added expense summary

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,56 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'a pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e2',
+        description: 'a pair of trousers',
+        amount: 89.99,
+        date: new Date('2021-12-05')
+    },
+    {
+        id: 'e3',
+        description: 'some bananas',
+        amount: 5.99,
+        date: new Date('2021-12-01')
+    },
+    {
+        id: 'e4',
+        description: 'a book',
+        amount: 14.99,
+        date: new Date('2022-02-19')
+    },
+    {
+        id: 'e5',
+        description: 'another book',
+        amount: 18.99,
+        date: new Date('2022-02-18')
+    }
+]
+
+function ExpensesOutput({ expenses,expensesPeriod }) {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700
+    }
+})
