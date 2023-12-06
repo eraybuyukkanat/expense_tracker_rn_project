73cc00d1440a59722cca9913dd68819f5b0eefdb
eraybuyukkanat <erybykknt@gmail.com>
2023-12-06 15:01:59 +0300

    handled user input

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,5 @@
 import { useContext, useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
@@ -36,6 +36,7 @@ function ManageExpenses({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <TextInput />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
